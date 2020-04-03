7c11d4b32c31a37d6ff1611fd83e29b245a19515
zhijieXU <2412399060@qq.com>
2020-04-03 16:13:04 +0800

    Fosa通知管理者

---
+++ FOSA/FOSA/fosaNotificationManager.m
@@ -0,0 +1,21 @@
+//
+//  fosaNotificationManager.m
+//  FOSA
+//
+//  Created by hs on 2020/4/3.
+//  Copyright © 2020 hs. All rights reserved.
+//
+
+#import "fosaNotificationManager.h"
+
+@implementation fosaNotificationManager
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
