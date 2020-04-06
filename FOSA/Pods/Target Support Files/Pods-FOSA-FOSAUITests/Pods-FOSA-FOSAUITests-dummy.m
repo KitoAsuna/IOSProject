3b9bd8c026acf6f3b2bbea3c6fc5b84d21d86cc6
zhijieXU <2412399060@qq.com>
2020-04-06 13:16:16 +0800

    2020/04/06

---
+++ FOSA/Pods/Target Support Files/Pods-FOSA-FOSAUITests/Pods-FOSA-FOSAUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FOSA_FOSAUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FOSA_FOSAUITests
+@end
---
+++ FOSA/Pods/Target Support Files/Pods-FOSA/Pods-FOSA-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FOSA : NSObject
+@end
+@implementation PodsDummy_Pods_FOSA
+@end
